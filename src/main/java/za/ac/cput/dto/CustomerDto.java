f379192bc7a1f5bbc5eead5a5deddf158666eeaa
Natheer <Natheer.Shade@csgi.com>
2024-10-18 00:31:56 +0200

    Improve Security

--- src/main/java/za/ac/cput/dto/CustomerDto.java
+++ src/main/java/za/ac/cput/dto/CustomerDto.java
@@ -5,13 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthenticationRequestDto {
+public class CustomerDto {
 
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
+    private String mobileNumber;
+    private LocalDate dateOfBirth;
 
 }
